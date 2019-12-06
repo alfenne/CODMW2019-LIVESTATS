d0776f57863dd63346c1be417153d574d43c4d31
Alex Fennell <49566237+fennellSAP@users.noreply.github.com>
2019-12-06 17:31:12 -0500

    adding websocket js script

---
+++ static/js/websocket.js
@@ -0,0 +1,25 @@
+function openWebsocket() {
+	console.log("Opening websocket");
+	var endpoint = "wss://mgwws.hana.ondemand.com/endpoints/v1/ws";
+	var topic = "in/nait/alex/codStats";
+	var ws = new WebSocket(endpoint);
+
+	ws.onopen = function () {
+		var subscribe = '{"subscribe":"' + topic + '"}';
+		ws.send(subscribe);
+	};
+
+	var self = this;
+	ws.onclose = function () {
+		console.log("Closing websocket")
+		self.openWebsocket();
+	};
+
+	ws.onmessage = function (evt) {
+		console.log("Receiving message");
+        var msg = JSON.parse(evt.data);
+        console.log(msg);
+        incrementCard();
+	};
+}
+openWebsocket(); 
