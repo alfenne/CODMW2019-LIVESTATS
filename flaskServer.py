f40d7bfd29edddb5e76a4df830e0c9458c95fcc0
fennellSAP <alexander.fennell@sap.com>
2019-12-11 18:43:57 -0500

    adding server side error handling

--- flaskServer.py
+++ flaskServer.py
@@ -17,37 +17,43 @@ def index():
 @app.route('/getStats', methods=['GET'])
 def getStats():
 
-
-    requests.post("https://api.dreamteam.gg/games/cod/players/xbl/import%20winner/update")
-    time.sleep(5)
-    response = requests.get("https://api.dreamteam.gg/games/cod/players/xbl/import%20winner/stats")
-    jsonData = json.loads(response.text)
-
-    statsDict = {}
-
-    kdSpread = jsonData['weekly']['all']['kd_ratio']
-    scorePerMinute = jsonData['weekly']['all']['score_per_minute']
-    scorePerGame = jsonData['weekly']['all']['score_per_game']
-    wlRatio = jsonData['weekly']['all']['wl_ratio']
-    longestStreak = jsonData['weekly']['all']['longest_streak']
-    killsPerGame = jsonData['weekly']['all']['kills'] / jsonData['weekly']['all']['matches_played']
-    killsPerMinute = jsonData['weekly']['all']['kills'] / (jsonData['weekly']['all']['time_played'] / 60)
-    accuracy = jsonData['weekly']['all']['accuracy']
-    atKdSpread = jsonData['lifetime']['all']['kd_ratio']
+    try:
 
 
-    statsDict['kdSpread'] = kdSpread
-    #statsDict['kdSpread'] = random.uniform(0, 1)
-    statsDict['scorePerMinute'] = scorePerMinute
-    statsDict['scorePerGame'] = scorePerGame
-    statsDict['wlRatio'] = wlRatio
-    statsDict['longestStreak'] = longestStreak
-    statsDict['killsPerGame'] = killsPerGame
-    statsDict['killsPerMinute'] = killsPerMinute
-    statsDict['accuracy'] = accuracy
-    statsDict['atKdSpread'] = atKdSpread
+        requests.post("https://api.dreamteam.gg/games/cod/players/xbl/import%20winner/update")
+        time.sleep(5)
+        response = requests.get("https://api.dreamteam.gg/games/cod/players/xbl/import%20winner/stats")
+        jsonData = json.loads(response.text)
 
-    return jsonify(statsDict)
+        statsDict = {}
+
+        kdSpread = jsonData['weekly']['all']['kd_ratio']
+        scorePerMinute = jsonData['weekly']['all']['score_per_minute']
+        scorePerGame = jsonData['weekly']['all']['score_per_game']
+        wlRatio = jsonData['weekly']['all']['wl_ratio']
+        longestStreak = jsonData['weekly']['all']['longest_streak']
+        killsPerGame = jsonData['weekly']['all']['kills'] / jsonData['weekly']['all']['matches_played']
+        killsPerMinute = jsonData['weekly']['all']['kills'] / (jsonData['weekly']['all']['time_played'] / 60)
+        accuracy = jsonData['weekly']['all']['accuracy']
+        atKdSpread = jsonData['lifetime']['all']['kd_ratio']
+
+
+        statsDict['kdSpread'] = kdSpread
+        #statsDict['kdSpread'] = random.uniform(0, 1)
+        statsDict['scorePerMinute'] = scorePerMinute
+        statsDict['scorePerGame'] = scorePerGame
+        statsDict['wlRatio'] = wlRatio
+        statsDict['longestStreak'] = longestStreak
+        statsDict['killsPerGame'] = killsPerGame
+        statsDict['killsPerMinute'] = killsPerMinute
+        statsDict['accuracy'] = accuracy
+        statsDict['atKdSpread'] = atKdSpread
+
+        return jsonify(statsDict)
+    
+    except:
+
+        return "error"
 
 
 
